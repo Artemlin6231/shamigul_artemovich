5c7fb67056bfd0c0f7417397e1546254eb812f35
Shamighoul <71729439+Shamighoul@users.noreply.github.com>
2021-04-25 16:09:25 +0300

    Update нидлман.py

--- нидлман.py
+++ нидлман.py
@@ -1,4 +1,4 @@
-def s(x, y, i, j):
+def f(x, y, i, j):
     if x[i] != y[j]:
         return 1
     else:
@@ -12,7 +12,7 @@ def way( TABLE, m, n):
     else:
         insert = 10**9
     if m != 0 and n != 0:
-        align = TABLE[m - 1][n - 1] + s(x, y, m - 1, n - 1)
+        align = TABLE[m - 1][n - 1] + f(x, y, m - 1, n - 1)
     else:
         align = 10**9
     if m != 0:
@@ -23,21 +23,24 @@ def way( TABLE, m, n):
     choosen = min(insert, align, delete)
 
     if choosen == insert:
-        sequence2.append(str(y[n - 1]))
-        sequence1.append('_')
+        alignment_sequence2.append(str(y[n - 1]))
+        alignment_sequence1.append('_')
         palki.append(' ')
         return way(TABLE, m, n - 1)
 
     elif choosen == align:
-        sequence2.append(str(y[n - 1]))
-        sequence1.append(str(x[m - 1]))
-        palki.append('|')
+        alignment_sequence2.append(str(y[n - 1]))
+        alignment_sequence1.append(str(x[m - 1]))
+        if str(y[n - 1])==str(x[m - 1]):
+            palki.append('|')
+        else:
+            palki.append(' ')
 
         return way(TABLE, m - 1, n - 1)
 
     elif choosen== delete:
-        sequence2.append("_")
-        sequence1.append(str(x[m - 1]))
+        alignment_sequence2.append("_")
+        alignment_sequence1.append(str(x[m - 1]))
         palki.append(' ')
 
         return way(TABLE, m - 1, n)
@@ -56,18 +59,20 @@ def alignment(x,y):
     for i in range(1, m+1):
         for j in range(1, n+1):
             TABLE[i][j] = min(
-                TABLE[i - 1][j - 1] + s(x, y, i - 1, j - 1),
+                TABLE[i - 1][j - 1] + f(x, y, i - 1, j - 1),
                 TABLE[i - 1][j] + 1,
                 TABLE[i][j - 1] + 1,
             )
     for i in range(m + 1):
         print(TABLE[i])
     way(TABLE, m, n)
-    return (TABLE[m][n],sequence1[::-1],sequence2[::-1],palki[::-1])
-x = 'TGACGTGCAC'#input('Введите последователльность:')
-y ='TCGACGTCAAC'#input('Введите вторую последователльность:')
-sequence1=[]
-sequence2 = []
+    return (TABLE[m][n],alignment_sequence1[::-1],alignment_sequence2[::-1],palki[::-1])
+
+
+x ='TTAGCTGCTTAGA'.upper()#input('Введите последователльность:')
+y ='AGCTAGTTgaT'.upper()#input('Введите вторую последователльность:')
+alignment_sequence1=[]
+alignment_sequence2 = []
 palki=[]
 min_edit, steps,steps2,palki = alignment(x,y)
 print(''.join(steps))
