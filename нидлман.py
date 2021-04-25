d8cfd55c133c356649945bfac16b494201a13f60
Artemlin6231 <71922548+Artemlin6231@users.noreply.github.com>
2021-04-25 15:05:38 +0300

    Update нидлман.py

--- нидлман.py
+++ нидлман.py
@@ -1,68 +1,75 @@
 def s(x, y, i, j):
-    '''
-
-    '''
     if x[i] != y[j]:
         return 1
     else:
         return 0
-#
-def find_solution( TABLE, m, n):
+
+def way( TABLE, m, n):
     if m == 0 and n == 0:
         return
     if n != 0:
         insert = TABLE[m][n - 1] + 1
     else:
-        insert = float("inf")
+        insert = 10**9
     if m != 0 and n != 0:
         align = TABLE[m - 1][n - 1] + s(x, y, m - 1, n - 1)
     else:
-        align = float("inf")
+        align = 10**9
     if m != 0:
         delete = TABLE[m - 1][n] + 1
     else:
-    delete = float("inf")
+        delete = 10**9
 
     choosen = min(insert, align, delete)
 
     if choosen == insert:
-        solution.append("insert_" + str(y[n - 1]))
-        return find_solution(TABLE, m, n - 1)
+        sequence2.append(str(y[n - 1]))
+        sequence1.append('_')
+        palki.append(' ')
+        return way(TABLE, m, n - 1)
 
     elif choosen == align:
-        solution.append("align_" + str(y[n - 1]))
-        return find_solution(TABLE, m - 1, n - 1)
+        sequence2.append(str(y[n - 1]))
+        sequence1.append(str(x[m - 1]))
+        palki.append('|')
+
+        return way(TABLE, m - 1, n - 1)
 
     elif choosen== delete:
-        solution.append("remove_" + str(x[m - 1]))
-        return find_solution(TABLE, m - 1, n)
+        sequence2.append("_")
+        sequence1.append(str(x[m - 1]))
+        palki.append(' ')
+
+        return way(TABLE, m - 1, n)
 
 def alignment(x,y):
-    n = len(y)
     m = len(x)
-    OPT = [[0 for r in range(n + 1)] for jy in range(m + 1)]
+    print(m)
+    n = len(y)
+    print(n)
+    TABLE = [[0 for r in range(n + 1)] for jy in range(m + 1)]
 
-    for i in range(1, m + 1):
-        OPT[i][0] = i
-
-    for j in range(1, n + 1):
-        OPT[0][j] = j
-
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            OPT[i][j] = min(
-                OPT[i - 1][j - 1] + s(x, y, i - 1, j - 1),
-                OPT[i - 1][j] + 1,
-                OPT[i][j - 1] + 1,
+    for i in range(1, m+1):
+        TABLE[i][0] = i
+    for j in range(1,n + 1):
+        TABLE[0][j] = j
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            TABLE[i][j] = min(
+                TABLE[i - 1][j - 1] + s(x, y, i - 1, j - 1),
+                TABLE[i - 1][j] + 1,
+                TABLE[i][j - 1] + 1,
             )
-
-    find_solution(OPT, m, n)
-
-    return (OPT[m][n],solution[::-1])
-x = 'TGACGTGC'
-y = 'TCGACGTCA'
-solution = []
-print('We we want to transform: ' + x + ' to: ' + y)
-min_edit, steps = alignment(x,y)
-print('Minimum amount of edit steps are: ' + str(min_edit))
-print('And the way to do it is: ' + str(steps))
+    for i in range(m + 1):
+        print(TABLE[i])
+    way(TABLE, m, n)
+    return (TABLE[m][n],sequence1[::-1],sequence2[::-1],palki[::-1])
+x = 'TGACGTGCAC'#input('Введите последователльность:')
+y ='TCGACGTCAAC'#input('Введите вторую последователльность:')
+sequence1=[]
+sequence2 = []
+palki=[]
+min_edit, steps,steps2,palki = alignment(x,y)
+print(''.join(steps))
+print(''.join(palki))
+print(''.join(steps2))
